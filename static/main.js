78c971de931352a1906a69b725b2061782bb7c3d
jacobbridges <him@jacobandkate143.com>
2015-10-10 21:48:51 -0400

    changed to dynamic jQuery event listeners for when posts are edited/added

--- static/main.js
+++ static/main.js
@@ -17,21 +17,15 @@
 	  },
 	  hide: function() {
 	    $('#shady').hide().empty();
-	  },
-	  addListener: function() {
-	  	lightboxy.init();
-	  	$('.plugin-mtg-a').click(function() {
-	    lightboxy.show($(this).attr('data-cardurl'))
-	      .click(function() {
-	        lightboxy.hide();
-	      });
-	  	});
 	  }
 	}
 
-	$(window).on('action:ajaxify.end', lightboxy.addListener);
-	$(window).on('action:ajaxify.dataLoaded', lightboxy.addListener);
-	$(window).on('action:ajaxify.contentLoaded', lightboxy.addListener);
-	// $(window).on('action:post.edit', lightboxy.addListener());
+	$(document).on('click', '.plugin-mtg-a', function() {
+		lightboxy.init();
+		lightboxy.show($(this).attr('data-cardurl'))
+      .click(function() {
+        lightboxy.hide();
+      });
+	});
 
 }());
